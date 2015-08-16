01f4bee02fb06fea0a40b8a437c2bd5ef0e4f3dc
ZengZhenwei <771875797@qq.com>
2015-08-16 13:02:57 +0800

    补充

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -7,7 +7,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 
-	
+	/**
+	 * Province表建表语句
+	 */
+	private static final String CREATE_PROVINCE="create table Province("
+			+"id integer primary key autoincrement,"
+			+"province_name text,"
+			+"province_code text)";
+	/**
+	 * City表建表语句
+	 */
+	private static final String CREATE_CITY="create table City("
+			+"id integer primary key autoincrement,"
+			+"city_name text,"
+			+"city_code text,"
+			+"province_id integer)";
+	/**
+	 * County表建表语句
+	 */
+	private static final String CREATE_COUNTY="create tacle County("
+			+"id integer primary key autoincrement,"
+			+"county_name text,"
+			+"county_code text,"
+			+"city_id integer)";
 	public CoolWeatherOpenHelper(Context context, String name,
 			CursorFactory factory, int version) {
 		super(context, name, factory, version);
@@ -17,7 +39,9 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-		
+		db.execSQL(CREATE_PROVINCE);//创建Province表
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
 	}
 
 	@Override
